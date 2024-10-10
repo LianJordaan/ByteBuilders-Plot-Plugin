6fa38d6cc5553d01fd49e565d188e46573b48a7d
LianJordaan <lianjordaan30052005@gmail.com>
2024-10-10 14:18:54 +0200

    Added some code to limit entities in the code world as well.

---
+++ src/main/java/io/github/lianjordaan/bytebuildersplotplugin/plotLimits/entityLoop.java
@@ -0,0 +1,48 @@
+package io.github.lianjordaan.bytebuildersplotplugin.plotLimits;
+
+import io.github.lianjordaan.bytebuildersplotplugin.ByteBuildersPlotPlugin;
+import io.github.lianjordaan.bytebuildersplotplugin.utils.LocationUtils;
+import io.github.lianjordaan.bytebuildersplotplugin.worldedit.LocationClamper;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class entityLoop {
+    public static void startLoop() {
+        try {
+            new BukkitRunnable() {
+                public void run() {
+                    for (World world : Bukkit.getWorlds()){
+                        if (!world.getName().startsWith("dim-")){
+                            continue;
+                        }
+                        if (world.getName().equalsIgnoreCase("dim-code")) {
+                            for (Entity entity : world.getEntities()) {
+                                if (entity instanceof Player) {
+                                    continue;
+                                }
+                                if (!LocationUtils.isWithinCodeBounds(entity.getLocation())) {
+                                    entity.teleport(LocationClamper.clampLocationToCodeBounds(entity.getLocation()));
+                                }
+                            }
+                        } else {
+                            for (Entity entity : world.getEntities()) {
+                                if (entity instanceof Player) {
+                                    continue;
+                                }
+                                if (!LocationUtils.isWithinPlotBounds(entity.getLocation())) {
+                                    entity.teleport(LocationClamper.clampLocationToPlotBounds(entity.getLocation()));
+                                }
+                            }
+                        }
+                    }
+                }
+            }.runTaskTimer(ByteBuildersPlotPlugin.getPlugin(ByteBuildersPlotPlugin.class), 1, 1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
